09b0e7e23975c089f6e9803cec749220d3c8253b
jrodriguezo <j.rodriguezo@alumnos.upm.es>
2022-01-30 12:27:18 +0100

    Deleting unused files and removing comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1>
+        Hello world!
+      </h1>
+    </div>
   );
 }
 
